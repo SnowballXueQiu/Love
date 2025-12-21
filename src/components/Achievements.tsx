94a8b27c7de3c3809723b5fd4cc62cd77244044a
Snowball_233 <wangrb.qq@outlook.com>
2025-12-21 23:10:34 +0800

    lint

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -234,7 +234,7 @@ export default function Achievements({ settings, currentUser }: AchievementsProp
                             
                             {currentUser && (
                                 <button 
-                                    onClick={(e) => {
+                                    onClick={(e) => {                               
                                         e.stopPropagation();
                                         handleDelete(item.id);
                                     }}
